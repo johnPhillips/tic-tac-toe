16d9cf8705487a55ccd2fdce58c89a9cf8297fcf
John Phillips <john.phillips@hivehome.com>
2020-07-13 11:53:44 +0100

    Working noughts and crosses with reset

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const initialState = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function App() {
+  const [getCells, setCells] = useState(initialState);
+  const [isX, setIsX] = useState(true);
+
+  const handleClick = ([rowNumber, colNumber]) => {
+    // return if cell is already full
+    if (getCells[rowNumber][colNumber]) return;
+
+    // get all cells
+    const cells = [...getCells];
+    // find the row
+    let row = [...cells[rowNumber]];
+    // update the row
+    row[colNumber] = isX ? "X" : "O";
+    // update the cells
+    cells[rowNumber] = row;
+
+    setCells(cells);
+    setIsX(!isX);
+  };
+
+  const handleReset = () => setCells(initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board">
+        {getCells.map((row, i) => {
+          return (
+            <div className="row" id={i}>
+              {row.map((cell, k) => (
+                <div
+                  className="cell"
+                  id={k}
+                  onClick={() => {
+                    handleClick([i, k]);
+                  }}
+                >
+                  {cell && String(cell)}
+                </div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
